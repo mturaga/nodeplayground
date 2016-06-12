15b6755fe82a9cdde79423c803dcaaf6aefc8e4c
mturaga <maruthi.turaga@gmail.com>
2016-06-12 17:10:04 -0500

    Added from VSCode

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ var app = express();
 
 var port = process.env.ENV || 3000;
 
+ app.use(express.static('public'));
+ app.use(express.static("src/views"));
+
+app.get("/books", function(req, res) {
+    res.send('Hello Books!!!');
+})
+
 app.listen(port, function (err) {
     console.log('Running at port ' + port);
 });
+
+
 
